5a087b50c50ef3cbb1d45ac08f04eadf24a69661
deandac <deandac805@gmail.com>
2023-12-18 16:16:27 -0800

    final

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,32 +1,109 @@
-// store api as queryurl
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
-
-// Perform a GET request to the query URL.
-d3.json(queryUrl).then(function(data) {
-	// Send the data.features object to the createFeatures function.
-	createFeatures(data.features);
-});
-
-// Define the createFeatures function first
-function createFeatures(features)
-
-
-
-// create tile for for map background
+// Step 1: tile layer for background map
 console.log("Step 1 working");
 
-// create tile layer for background map
+// Step 2: Define the basemap using Leaflet's tileLayer
 let basemap = L.tileLayer(
-    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'",
-    {
-      attribution:
-        'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-    });
+  "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'",
+  {
+    attribution:
+      'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+  });
 
-    
-    let map = L.map("map", {
-        center: [40.7, -94.5], 
-        zoom: 3
-    });
-    
-      
+// Step 3: Create a map object and set its options
+let map = L.map("map", {
+  center: [0, 0],
+  zoom: 2
+});
+
+// Step 4: Add the basemap tile layer to the map
+basemap.addTo(map);
+
+// Step 5: Make an AJAX call to retrieve earthquake geoJSON data
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+
+  // Step 6: Define a function to determine the style of each earthquake marker
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // Step 7: Define a function to determine the color of the marker based on earthquake depth
+  function getColor(depth) {
+    switch (true) {
+      case depth > 90:
+        return "#ea2c2c";
+      case depth > 70:
+        return "#ea822c";
+      case depth > 50:
+        return "#ee9c00";
+      case depth > 30:
+        return "#eecc00";
+      case depth > 10:
+        return "#d4ee00";
+      default:
+        return "#98ee00";
+    }
+  }
+
+  // Step 8: Define a function to determine the radius of the earthquake marker based on its magnitude
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+
+  // Step 9: Add a GeoJSON layer to the map
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+        "Magnitude: "
+        + feature.properties.mag
+        + "<br>Depth: "
+        + feature.geometry.coordinates[2]
+        + "<br>Location: "
+        + feature.properties.place
+      );
+    }
+  }).addTo(map);
+
+  // Step 10: Create a legend control object
+  let legend = L.control({
+    position: "bottomright"
+  });
+
+  // Step 11: Add details to the legend
+  legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+
+    let grades = [-10, 10, 30, 50, 70, 90];
+    let colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+    for (let i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+        + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  // Step 12: Add the legend to the map
+  legend.addTo(map);
+});
